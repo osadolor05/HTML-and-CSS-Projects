7dffa683296fe8212c665d0ccaf6119b995c901b
Osadolor Oriabor <osadolor05@gmail.com>
2026-05-20 03:51:46 +0100

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Source Attribute Assignment");
+
+function My_First_Function() {
+			var String = "This is my first javascript fxn!";
+			var result = String.fontcolor("blue");
+			document.getElementById("fxn").innerHTML = result;
+		}
